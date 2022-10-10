42b250f1bf5bb062e41776796bfac9bcbb1f4084
xTony0x <jabelteton@correo.utl.edu.gt>
2022-10-09 18:25:19 -0600

    prueba

---
+++ Pokédex_Anthony_Belteton_1171422/Pokédex_Anthony_Belteton_1171422/MyForm.cpp
@@ -0,0 +1,14 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	PokédexAnthonyBelteton1171422::MyForm form;
+	Application::Run(% form);
+	return 0;
+}
+
+
